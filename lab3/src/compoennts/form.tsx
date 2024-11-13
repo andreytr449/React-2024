1bbc171c57a91aba3652d222e7a90acd297a4e5f
Андрій Трохимчук <andreytr449@gmail.com>
2024-11-13 19:23:36 +0200

    feat: created form schema

--- lab3/src/compoennts/form.tsx
+++ lab3/src/compoennts/form.tsx
@@ -1,11 +1,32 @@
 import {Container} from "./container.tsx";
+import {FormProvider, useForm} from "react-hook-form";
+import {formSchema, formType} from "../constans/schema/form-schema.ts";
+import {yupResolver} from "@hookform/resolvers/yup";
 
-export function Form(){
+export function Form() {
+    const form = useForm<formType>({
+        defaultValues: {
+            fullName: '',
+            contact: '',
+            description: '',
+            formatType: '',
+            topic: ''
+        },
+        resolver: yupResolver(formSchema)
+    })
+    const onSubmit = (data : formType) => {
+        console.log(data)
+    }
+
     return (
         <Container>
-            <div className=''>
-                123
-            </div>
+            <FormProvider  {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)}>
+                    <div className=''>
+                        123
+                    </div>
+                </form>
+            </FormProvider>
         </Container>
     )
 }
